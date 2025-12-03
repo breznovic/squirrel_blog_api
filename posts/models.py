b883bee1807acb0b5e42b82aa2ba490f547ec303
breznovic <slymarkel@gmail.com>
2025-12-03 14:10:10 +0300

    feat: add API first version

---
+++ posts/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    image_url = models.URLField(
+        blank=True, null=True, default='https://static.vecteezy.com/system/resources/thumbnails/024/654/096/small/fluffy-squirrel-munching-on-acorns-in-tree-generated-by-ai-free-photo.jpg')
+    thumbnail_url = models.URLField(
+        blank=True, null=True, default='https://static.vecteezy.com/system/resources/thumbnails/024/654/096/small/fluffy-squirrel-munching-on-acorns-in-tree-generated-by-ai-free-photo.jpg')
+    published_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    author = models.CharField(max_length=100, default="Squirrel Rusty")
+    user_id = models.IntegerField(default=1)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            if not self.published_at:
+                self.published_at = timezone.now()
+        self.updated_at = timezone.now()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
